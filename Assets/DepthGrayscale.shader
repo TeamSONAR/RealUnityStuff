3b396eaf9446f1ff6b79797e9cff557c6e4d7a40
colinpate <pate.colin@gmail.com>
2016-11-17 10:34:06 -0800

    Summary
    Description

--- Assets/DepthGrayscale.shader
+++ Assets/DepthGrayscale.shader
@@ -1,44 +1,56 @@
 ﻿Shader "Custom/DepthGrayscale" {
-	SubShader{
-		Tags{ "RenderType" = "Opaque" }
+SubShader{
+Tags{ "RenderType" = "Opaque" }
 
-		Pass{
-		CGPROGRAM
+Pass{
+CGPROGRAM
 #pragma vertex vert
 #pragma fragment frag
 #include "UnityCG.cginc"
 
 		sampler2D _CameraDepthTexture;
 
-	struct v2f {
-		float4 pos : SV_POSITION;
-		float4 scrPos:TEXCOORD1;
-	};
+struct v2f {
+	float4 pos : SV_POSITION;
+	float4 scrPos:TEXCOORD1;
+	float depth : TEXCOORD0;
+};
+
+struct fragOut
+{
+	half4 color : SV_Target;
+	float depth : SV_Depth;
+};
 
 	//Vertex Shader
-	v2f vert(appdata_base v) {
-		v2f o;
-		o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
-		o.scrPos = ComputeScreenPos(o.pos);
-		//for some reason, the y position of the depth texture comes out inverted
-		//o.scrPos.y = 1 - o.scrPos.y;
-		return o;
-	}
+v2f vert(appdata_base v) {
+	v2f o;
+	o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+	o.scrPos = ComputeScreenPos(o.pos);
+	UNITY_TRANSFER_DEPTH(o.depth);
+	return o;
+}
 
-	//Fragment Shader
-	half4 frag(v2f i) : COLOR{
-		float depthValue = Linear01Depth(tex2Dproj(_CameraDepthTexture, UNITY_PROJ_COORD(i.scrPos)).r);
-		half4 depth;
+//Fragment Shader
+fragOut frag(v2f i){
+	fragOut o;
 
-		depth.r = depthValue;
-		depth.g = depthValue;
-		depth.b = depthValue;
+	float depthValue = Linear01Depth(tex2Dproj(_CameraDepthTexture, UNITY_PROJ_COORD(i.scrPos)).r);
+	//half depth;
 
-		depth.a = 1;
-		return depth;
-	}
-		ENDCG
-	}
-	}
-		FallBack "Diffuse"
+	o.color.b = fmod(depthValue * 256, 1);
+	o.color.g = floor(depthValue * 256)/256;
+	//UNITY_OUTPUT_DEPTH(i.depth);
+	//depth.g = depthValue;
+	//depth.b = depthValue;
+	o.depth = depthValue;
+
+	//depth.a = 1;
+	return o;
+}
+
+ENDCG
+}
+}
+FallBack "Diffuse"
 }
--- Assets/PostProcessDepthGrayscale.cs
+++ Assets/PostProcessDepthGrayscale.cs
@@ -1,41 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Runtime.InteropServices;
-//using OpenGL32.dll;
-
-
 //so that we can see changes we make without having to run the game
 
 [ExecuteInEditMode]
 public class PostProcessDepthGrayscale : MonoBehaviour
 {
+    [DllImport("RenderingPlugin")]
+    private static extern void SetTimeFromUnity(float t);
+
+    [DllImport("RenderingPlugin")]
+    private static extern System.IntPtr GetRenderEventFunc();
+
+    [DllImport("RenderingPlugin")]
+    private static extern int GetError();
+
+    [DllImport("RenderingPlugin")]
+    private static extern int GetPBVal(int num);
+
+    [DllImport("RenderingPlugin")]
+    private static extern int SetupReadPixels();
+
+    [DllImport("RenderingPlugin")]
+    private static extern void UnmapFile();
+
+    [DllImport("RenderingPlugin")]
+    private static extern void SetDepthF(int GDF);
+
 
     public Material mat;
+
+    int GL_DEPTH_COMPONENT16 = 0x81A5;
+    int GL_DEPTH_COMPONENT = 0x1902;
+    int GL_RED = 0x1903;
+    int GL_DEPTH_COMPONENT24 = 0x81A6;
+    int GL_DEPTH_COMPONENTS = 0x8284;
+    int GL_DEPTTH24_STENCIL8 = 0x88F0;
+    int OldFoo = 0;
+    int pwidth;
 
     void Start()
     {
         GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
-        //GL.
-        
+        Vector3 extent = GetComponent< Camera>().ViewportToScreenPoint(new Vector3(0, 0, 0));
+        print(extent.x.ToString());
+        print(extent.y.ToString());
+    }
+
+    void OnEnable()
+    {
+        int cwidth = GetComponent<Camera>().pixelWidth;
+        pwidth = cwidth;
+        int cheight = GetComponent<Camera>().pixelHeight;
+
+        print((cwidth * cheight).ToString());
+        int outval = SetupReadPixels();
+        print(outval.ToString());
+        print("Mapping");
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
+        //print("onrender");
+        
         Graphics.Blit(source, destination, mat);
-        //mat is the material which contains the shader
-        //we are passing the destination RenderTexture to
+    }
 
-        //Texture2D MyTex = new Texture2D(source.width, source.height);
-        System.IntPtr Zoop;
-        Zoop = source.GetNativeDepthBufferPtr();
-        //source.depth = 16;
-        //print(Zoop.ToInt32().ToString());
-        print(source.depth.ToString());
+    void OnPostRender()
+    {
+        //print("onpostrender");
+        //GL.Viewport(new Rect(0, 0, 640, 480));
+        SetDepthF(0x80E1);
+        SetTimeFromUnity(Time.timeSinceLevelLoad);
 
-        //RenderTexture.active = source;
-        //MyTex.ReadPixels(new Rect(0, 0, source.width, source.height), 0, 0);
-        //MyTex.Apply();
-        //byte[] bytes;
-        //bytes = MyTex.EncodeToPNG();
+        GL.IssuePluginEvent(GetRenderEventFunc(), 1);
+    }
+
+    void OnDisable()
+    {
+        print("unmapping");
+        UnmapFile();
     }
 }
+
